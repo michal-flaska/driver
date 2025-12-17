68ca29b0412b7f232595ffb9d665332e1ed6b93f
pilot2254 <109202734+pilot2254@users.noreply.github.com>
2025-12-17 16:42:34 +0100

    fix missing includes and declarations in driver.h

--- driver.h
+++ driver.h
@@ -1,13 +1,69 @@
 #pragma once
 #include <ntddk.h>
+#include <wdm.h>
+#include <ntdef.h>
+#include <ntstrsafe.h>
 
-// IOCTL codes for communication
+// Undocumented structures
+typedef struct _SYSTEM_PROCESS_INFORMATION {
+    ULONG NextEntryOffset;
+    ULONG NumberOfThreads;
+    BYTE Reserved1[48];
+    UNICODE_STRING ImageName;
+    KPRIORITY BasePriority;
+    HANDLE UniqueProcessId;
+    PVOID Reserved2;
+    ULONG HandleCount;
+    ULONG SessionId;
+    PVOID Reserved3;
+    SIZE_T PeakVirtualSize;
+    SIZE_T VirtualSize;
+    ULONG Reserved4;
+    SIZE_T PeakWorkingSetSize;
+    SIZE_T WorkingSetSize;
+    PVOID Reserved5;
+    SIZE_T QuotaPagedPoolUsage;
+    PVOID Reserved6;
+    SIZE_T QuotaNonPagedPoolUsage;
+    SIZE_T PagefileUsage;
+    SIZE_T PeakPagefileUsage;
+    SIZE_T PrivatePageCount;
+    LARGE_INTEGER Reserved7[6];
+} SYSTEM_PROCESS_INFORMATION, * PSYSTEM_PROCESS_INFORMATION;
+
+typedef enum _SYSTEM_INFORMATION_CLASS {
+    SystemProcessInformation = 5
+} SYSTEM_INFORMATION_CLASS;
+
+// External declarations
+NTKERNELAPI NTSTATUS NTAPI ZwQuerySystemInformation(
+    SYSTEM_INFORMATION_CLASS SystemInformationClass,
+    PVOID SystemInformation,
+    ULONG SystemInformationLength,
+    PULONG ReturnLength
+);
+
+NTKERNELAPI NTSTATUS NTAPI PsLookupProcessByProcessId(
+    HANDLE ProcessId,
+    PEPROCESS* Process
+);
+
+NTKERNELAPI VOID NTAPI KeStackAttachProcess(
+    PEPROCESS Process,
+    PKAPC_STATE ApcState
+);
+
+NTKERNELAPI VOID NTAPI KeUnstackDetachProcess(
+    PKAPC_STATE ApcState
+);
+
+// IOCTL codes
 #define IOCTL_BASE 0x800
 #define IOCTL_READ_MEMORY  CTL_CODE(FILE_DEVICE_UNKNOWN, IOCTL_BASE + 0x1, METHOD_BUFFERED, FILE_ANY_ACCESS)
 #define IOCTL_WRITE_MEMORY CTL_CODE(FILE_DEVICE_UNKNOWN, IOCTL_BASE + 0x2, METHOD_BUFFERED, FILE_ANY_ACCESS)
 #define IOCTL_GET_PROCESS  CTL_CODE(FILE_DEVICE_UNKNOWN, IOCTL_BASE + 0x3, METHOD_BUFFERED, FILE_ANY_ACCESS)
 
-// Structures for data exchange
+// Structures
 typedef struct _READ_WRITE_REQUEST {
     ULONG ProcessId;
     PVOID Address;
@@ -26,7 +82,6 @@ VOID DriverUnload(PDRIVER_OBJECT DriverObject);
 NTSTATUS CreateClose(PDEVICE_OBJECT DeviceObject, PIRP Irp);
 NTSTATUS IoControl(PDEVICE_OBJECT DeviceObject, PIRP Irp);
 
-// Memory operations
 NTSTATUS ReadProcessMemory(ULONG ProcessId, PVOID Address, PVOID Buffer, SIZE_T Size);
 NTSTATUS WriteProcessMemory(ULONG ProcessId, PVOID Address, PVOID Buffer, SIZE_T Size);
 NTSTATUS GetProcessIdByName(PWCH ProcessName, PULONG ProcessId);
